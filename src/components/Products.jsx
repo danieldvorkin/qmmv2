7392cf325985d769f2db8d2ab88ebb41784b3f73
Daniel Dvorkin <dvorkin212@gmail.com>
2023-11-25 20:47:52 -0500

    some shop main page updates for filtering and searching combined

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,37 +5,46 @@ import { Badge, Col, Row } from "react-bootstrap";
 import Product from "./Product";
 
 const Products = (props) => {
-  let { products, selectedFilter, resetFilter } = props;
-  
+  let { products, selectedFilter, resetFilter, typeFilter, resetTypeFilter, emptyCategoryError } = props;
+  console.log("Selected Filter: ", selectedFilter);
   return (
-    <>
-      {!products ? (
-        <Text>No Products Available</Text>
-      ) : (
-        <div>
-          {selectedFilter?.name?.length > 0 && (
-            <Text style={{fontSize: 20, marginLeft: 10, marginBottom: 10}}>
-              <Badge bg="info" style={{paddingRight: 5}}>
-                {` ${selectedFilter?.name}`}
-                <Button className="bp4-minimal" style={{ backgroundColor: 'transparent' }} icon="cross" onClick={resetFilter} />
-              </Badge>
-            </Text>
-          )}
-          
-          <Row>
-            {products && products.map((product) => {
-              if(product?.price > 0 && product?.inventory > 0) {
-                return (
-                  <Col xl={3} lg={4} md={6} xs={12} key={product.id}>
-                    <Product product={product} category={selectedFilter} />
-                  </Col>
-                )
-              }
-            })}
-          </Row>
-        </div>
-      )}
-    </>
+    <div>
+      <div style={{display: 'flex'}}>
+        {selectedFilter?.name?.length > 0 && (
+          <Text style={{fontSize: 20, marginLeft: 10, marginBottom: 10}}>
+            <Badge bg="info" style={{ paddingRight: 5, top: 2, position: 'relative' }}>
+              {` ${selectedFilter?.name}`}
+              <Button className="bp4-minimal" style={{ backgroundColor: 'transparent' }} icon="cross" onClick={resetFilter} />
+            </Badge>
+          </Text>
+        )}
+        {!!typeFilter && (
+          <Text style={{fontSize: 20, marginLeft: 10, marginBottom: 10}}>
+            <Badge bg="secondary" style={{paddingRight: 5, top: 2, position: 'relative'}}>
+              {typeFilter}
+              <Button className="bp4-minimal" style={{ backgroundColor: 'transparent' }} icon="cross" onClick={resetTypeFilter} />
+            </Badge>
+          </Text>
+        )}
+      </div>
+      
+      <Row>
+        {products && products.map((product) => {
+          if(product?.price > 0 && product?.inventory > 0) {
+            return (
+              <Col xl={3} lg={4} md={6} xs={12} key={product.id}>
+                <Product product={product} category={selectedFilter} />
+              </Col>
+            )
+          }
+        })}
+        {!!emptyCategoryError && (
+          <Col>
+            <Text ml={4}>{emptyCategoryError}</Text>
+          </Col>
+        )}
+      </Row>
+    </div>
   )
 }
 export default Products;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -9,6 +9,7 @@ import { featuredItems, getCategories, getCategory, getItem, getOrder, getOrders
 import loading from '../loading.svg';
 import CustomSlider from "../components/slider";
 import { ChevronUpIcon, CloseIcon } from "@chakra-ui/icons";
+import { AppToaster } from "../toast";
 
 const Shop = (props) => {
   const [queryParams, _] = useSearchParams();
@@ -16,11 +17,9 @@ const Shop = (props) => {
   const [filterObject, setFilterObject] = useState({});
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [baseProducts, setBaseProducts] = useState([]);
   const navigate = useNavigate();
-  const [loader, setLoader] = useState(true)
-  const [thcSliderValue, setThcSliderValue] = useState(50)
-  const [cbdSliderValue, setCbdSliderValue] = useState(50)
-  const [typeFilter, setTypeFilter] = useState('');
+  const [loader, setLoader] = useState(true);
   const containerRef = useRef(null);
   const [scrolledTo, setScrolledTo] = useState(false);
   const [showRecentlyBoughtBadge, setShowRecentlyBoughtBadge] = useState(true);
@@ -30,33 +29,143 @@ const Shop = (props) => {
   const [previousQuery, setPreviousQuery] = useState('');
   const [recentlyBought, setRecentlyBought] = useState({});
   const [orderCount, setOrderCount] = useState(0);
+  const [thcSliderValue, setThcSliderValue] = useState(50);
+  const [cbdSliderValue, setCbdSliderValue] = useState(50);
+  const [typeFilter, setTypeFilter] = useState('');
+  const [emptyCategoryError, setEmptyCategoryError] = useState(null);
+
+  useEffect(() => {
+    const intervalId = setInterval(fetchOrders, 30000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [orderCount]);
+
+  useEffect(() => {
+    if (!!recentlyBought.item) {
+      getItem(recentlyBought.item.slug).then((resp) => {
+        if (!!resp.id) {
+          setRecentlyBought({ ...recentlyBought, item: resp });
+        }
+      });
+    }
+  }, [orderCount]);
+
+  useEffect(() => {
+    setLoader(true);
+    
+    setTimeout(() => {
+      fetchOrders();
+    }, 5000);
+
+    const fetchData = async () => {
+      try {
+        const categoriesResponse = await getCategories();
+        setCategories(categoriesResponse);
+
+        const featuredItemsResponse = await featuredItems();
+        setProducts(featuredItemsResponse);
+        setBaseProducts(featuredItemsResponse);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const hash = window.location.hash;
+
+    if (hash && containerRef.current && products.length > 0 && !scrolledTo) {
+      const itemSlug = hash.substring(1);
+      const element = containerRef.current.querySelector(`#${itemSlug}`);
+
+      if (element) {
+        const offset = element.offsetTop;
+        containerRef.current.scrollTop = offset - 80;
+        setScrolledTo(true);
+      }
+    }
+  }, [products, scrolledTo]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoader(true);
+
+        let filteredProducts = baseProducts;
+
+        if (filterSlug?.length > 0) {
+          const categoryResponse = await getCategory(filterSlug);
+          filteredProducts = categoryResponse.items;
+          
+          if(filteredProducts?.length > 0){
+            setEmptyCategoryError(null)
+            setFilterObject(categoryResponse); // Assuming there's only one category item
+          } else {
+            AppToaster.show({ message: 'There are no products available in that category.' });
+            setEmptyCategoryError("No products available for the selected category. Please select a different category.")
+            resetFilter();
+          }
+        }
+
+        if (!!typeFilter && typeFilter.length > 0) {
+          filteredProducts = filteredProducts.filter((p) => p.strain_type.includes(typeFilter));
+        }
+
+        if (searchQuery.length > 0) {
+          filteredProducts = filteredProducts.filter((p) => nameCheck(p, searchQuery));
+        }
+
+        if(filteredProducts?.length <= 0){
+          setEmptyCategoryError("No products available....")
+        }
+
+        setProducts(filteredProducts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fetchData();
+  }, [filterSlug, typeFilter, searchQuery]);
+
+  const fetchFeaturedItems = async () => {
+    const resp = await featuredItems();
+    setProducts(resp);
+    setBaseProducts(resp);
+  };
 
   const fetchOrders = async () => {
-    if(!stopShowingRecentlyBought){
+    if (!stopShowingRecentlyBought) {
       try {
         setShowRecentlyBoughtBadge(false);
         let recentlyBoughtItem = null;
 
-        if(mostRecentOrders.length === 0){
+        if (mostRecentOrders.length === 0) {
           const resp = await getOrders(1, 'Order Delivered');
-      
+
           if (resp.orders.length > 0) {
             setMostRecentOrders(resp.orders);
-            recentlyBoughtItem = resp.orders[orderCount].line_items[0]
-            // setRecentlyBought(resp.orders[orderCount].line_items[0]);
+            recentlyBoughtItem = resp.orders[orderCount].line_items[0];
           }
         } else {
-          if(!!mostRecentOrders[orderCount]){
-            // setRecentlyBought(mostRecentOrders[orderCount].line_items[0]);
-            recentlyBoughtItem = mostRecentOrders[orderCount].line_items[0]
+          if (!!mostRecentOrders[orderCount]) {
+            recentlyBoughtItem = mostRecentOrders[orderCount].line_items[0];
           } else {
             setOrderCount(0);
-            // setRecentlyBought(mostRecentOrders[0].line_items[0]);
-            recentlyBoughtItem = mostRecentOrders[orderCount].line_items[0]
+            recentlyBoughtItem = mostRecentOrders[orderCount].line_items[0];
           }
         }
-        
-        if(recentlyBoughtItem?.item?.inventory > 0){
+
+        if (recentlyBoughtItem?.item?.inventory > 0) {
           setRecentlyBought(recentlyBoughtItem);
           setOrderCount((prevOrderCount) => prevOrderCount + 1);
         }
@@ -67,224 +176,81 @@ const Shop = (props) => {
       }
     }
   };
-  
-  useEffect(() => {
-    const intervalId = setInterval(fetchOrders, 30000);
-  
-    // Clean up the interval when the component unmounts
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [orderCount]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetchOrders();
-    }, 5000)
-  }, []);
-
-  useEffect(() => {
-    if(!!recentlyBought.item){
-      getItem(recentlyBought.item.slug).then((resp) => {
-        if(!!resp.id){
-          setRecentlyBought({ ...recentlyBought, item: resp });
-        }
-      })
-    }
-  }, [orderCount])
-
-  useEffect(() => {
-    let slug = queryParams.get('filter');
-    setFilterSlug(slug);
-
-    getCategories().then((resp) => setCategories(resp));
-    featuredItems().then((resp) => setProducts(resp));
-    
-    if(Object.keys(categories).length > 0){
-      let filterHash = Object.keys(categories).map((cat) => {
-        const list = categories[cat];
-        
-        return list.filter((item) => filterSlug === item.slug);
-      }).flat();
-      
-      setFilterObject(filterHash[0]);
-    }
-
-    setTimeout(() => {
-      setLoader(false);  
-    }, 1000);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [queryParams])
-
-  useEffect(() => {
-    if(filterSlug?.length > 0){
-      setLoader(true);  
-      getCategory(filterSlug).then((resp) => setProducts(resp.items));
-
-      let filterHash = Object.keys(categories).map((cat) => {
-        const list = categories[cat];
-        
-        return list.filter((item) => filterSlug === item.slug);
-      }).flat();
-      
-      setFilterObject(filterHash[0]);
-    }
-    setTimeout(() => {
-      setLoader(false);
-    }, 1000)
-    
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filterSlug])
-
-  useEffect(() => {
-    setLoader(true);
-    
-    if(!!typeFilter && typeFilter?.length > 0){
-      setProducts(products.filter((p) => p.strain_type === typeFilter));
-      setLoader(false);
-    } else {
-      featuredItems().then((resp) => {
-        setProducts(resp);
-        setLoader(false);
-      });
-    }
-  }, [typeFilter]);
-
-  useEffect(() => {
-    if(!!typeFilter)
-      setTypeFilter('');
-  }, [filterObject]);
 
   const resetFilter = () => {
     setLoader(true);
     setFilterSlug("");
     setFilterObject({});
-    navigate("/shop");
-    featuredItems().then((resp) => setProducts(resp));
-    setTimeout(() => {
-      setLoader(false);  
-    }, 1000);
-  }
+    resetTypeFilter();
+    fetchFeaturedItems();
+  };
 
-  
+  const resetTypeFilter = () => {
+    setLoader(true);
+    setTypeFilter('');
+    if(filterSlug?.length <= 0)
+      fetchFeaturedItems();
+  };
+
   const scrollToTop = () => {
     if (containerRef.current) {
       containerRef.current.scrollTop = 0;
     }
-  }
-
-  useEffect(() => {
-    const hash = window.location.hash;
-
-    if (hash && containerRef.current && products.length > 0 && !scrolledTo) {
-      const itemSlug = hash.substring(1);
-      const element = containerRef.current.querySelector(`#${itemSlug}`);
-
-      if (element) {
-        // Calculate the scroll position based on the element's offset
-        const offset = element.offsetTop;
-        containerRef.current.scrollTop = offset - 80;
-        setScrolledTo(true);
-      }
-    }
-  }, [products]);
-
-  useEffect(() => {
-    let debounceTimeout;
-  
-    const updateProducts = async (query) => {
-      if (query) {
-        let applicableItems = [];
-  
-        if (query?.length < previousQuery?.length) {
-          const resp = await featuredItems();
-          applicableItems = resp.filter((p) => nameCheck(p, query));
-        } else {
-          applicableItems = products.filter((p) => nameCheck(p, query));
-        }
-  
-        setProducts(applicableItems);
-        setLoader(false);
-      } else {
-        // If the search query is empty, fetch the default data (e.g., featured items)
-        const resp = await featuredItems();
-        setProducts(resp);
-        setLoader(false);
-      }
-    };
-  
-    const handleSearch = () => {
-      if (debounceTimeout) {
-        clearTimeout(debounceTimeout);
-      }
-  
-      debounceTimeout = setTimeout(() => {
-        updateProducts(searchQuery);
-      }, 500); // Adjust the delay as needed
-    };
-  
-    handleSearch();
-  
-    return () => {
-      if (debounceTimeout) {
-        clearTimeout(debounceTimeout);
-      }
-    };
-  }, [searchQuery]);
+  };
 
   const nameCheck = (product, query) => {
     let lcName = product?.name?.toLowerCase();
-
     return lcName.includes(query.toLowerCase());
-  }
+  };
 
   const onSearchChange = (e) => {
     const newSearchQuery = e.target.value;
-    
+
     setPreviousQuery(searchQuery);
     setLoader(true);
 
     setSearchQuery(newSearchQuery);
-  }
+  };
 
   const goToProduct = () => {
-    navigate("/products/" + recentlyBought.item?.slug)
-  }
+    navigate("/products/" + recentlyBought.item?.slug);
+  };
 
   const determineScale = () => {
-    if(!!recentlyBought && !!recentlyBought.item){
-      if(recentlyBought.item?.category?.type_of === "Strains")
+    if (!!recentlyBought && !!recentlyBought.item) {
+      if (recentlyBought.item?.category?.type_of === "Strains") {
         return "g";
+      }
     }
-  }
-  
-  return(
+  };
+
+  return (
     <div style={{ marginTop: 20 }}>
-      {!stopShowingRecentlyBought && showRecentlyBoughtBadge && !!recentlyBought.item && (
+      {false && !stopShowingRecentlyBought && showRecentlyBoughtBadge && !!recentlyBought.item && (
         <div className="recentlyBought">
           <Card>
             <CardBody>
               <Flex>
-                <Avatar name={recentlyBought && recentlyBought.item?.name} src={recentlyBought.item?.cover_photo}/>
-                <Box ml="3" onClick={() => goToProduct()} style={{cursor: 'pointer', width: '100%'}}>
+                <Avatar name={recentlyBought && recentlyBought.item?.name} src={recentlyBought.item?.cover_photo} />
+                <Box ml="3" onClick={() => goToProduct()} style={{ cursor: 'pointer', width: '100%' }}>
                   <Text fontWeight='bold'>
                     Someone Recently Bought
                   </Text>
                   {recentlyBought && (
-                    <Text fontSize='sm'>{recentlyBought?.quantity}{determineScale()} of {recentlyBought.item?.name}</Text>  
+                    <Text fontSize='sm'>{recentlyBought?.quantity}{determineScale()} of {recentlyBought.item?.name}</Text>
                   )}
                 </Box>
-                <CloseIcon style={{cursor: 'pointer', right: 0, position: 'relative' }} ml="3" onClick={() => setStopShowingRecentlyBought(true)}/>
+                <CloseIcon style={{ cursor: 'pointer', right: 0, position: 'relative' }} ml="3" onClick={() => setStopShowingRecentlyBought(true)} />
               </Flex>
             </CardBody>
           </Card>
         </div>
       )}
-      
+
       <div className="container">
         <Row>
-          <Col lg="12" style={{marginBottom: 20}}>
-            {Object.keys(categories).map((key) => 
+          <Col lg="12" style={{ marginBottom: 20 }}>
+            {Object.keys(categories).map((key) =>
               <DropdownButton
                 as={ButtonGroup}
                 id={"filter"}
@@ -292,17 +258,17 @@ const Shop = (props) => {
                 key={`dropdown-${key}`}
                 variant="outline-secondary"
                 title={key}
-                style={{marginRight: 5, border: 'none !important'}}
+                style={{ marginRight: 5, border: 'none !important' }}
               >
                 {categories[key].sort((a, b) => a.sort - b.sort).map((category) => {
                   return (
-                    <Dropdown.Item 
-                      eventKey={category.slug} 
+                    <Dropdown.Item
+                      eventKey={category.slug}
                       onClick={() => { setFilterSlug(category?.slug) }}
                       style={{ border: 'none' }}>{category.name}</Dropdown.Item>
                   )
                 })}
-                
+
               </DropdownButton>
             )}
             <DropdownButton
@@ -312,41 +278,40 @@ const Shop = (props) => {
               key={`dropdown-type`}
               variant="outline-secondary"
               title={"Strain Types"}
-              style={{marginRight: 5, border: 'none !important' }}
-              >
-                {["Indica", "Indica Hybrid", "Hybrid", "Sativa Hybrid", "Sativa"].map((option) => {
-                  return (
-                    <Dropdown.Item eventKey={option} value={typeFilter} onClick={(e) => setTypeFilter(e.target.text)} style={{ border: 'none' }}>{option}</Dropdown.Item>
-                  )
-                })}
-              </DropdownButton>
-              <Input id="shopSearchInput" name="searchItem" placeholder="Search..." onChange={onSearchChange}/>
+              style={{ marginRight: 5, border: 'none !important' }}
+            >
+              {["Indica", "Hybrid", "Sativa"].map((option) => {
+                return (
+                  <Dropdown.Item eventKey={option} value={typeFilter} onClick={(e) => setTypeFilter(e.target.text)} style={{ border: 'none' }}>{option}</Dropdown.Item>
+                )
+              })}
+            </DropdownButton>
+            <Input id="shopSearchInput" name="searchItem" placeholder="Search..." onChange={onSearchChange} />
           </Col>
-          <Col lg="12" style={{height: '82vh', overflowY: 'auto', width: '98%' }} ref={containerRef}>
-            <div fluid style={{marginTop: 20}}>
+          <Col lg="12" style={{ height: '82vh', overflowY: 'auto', width: '98%' }} ref={containerRef}>
+            <div fluid style={{ marginTop: 20 }}>
               {loader ? (
                 <div style={{ width: '100%' }}>
-                  <img style={{ margin: '0 auto' }} src={loading} alt={"loading"}/>
+                  <img style={{ margin: '0 auto' }} src={loading} alt={"loading"} />
                 </div>
               ) : (
                 <>
-                  {!!typeFilter && (
-                    <Text style={{fontSize: 20, marginLeft: 10, marginBottom: 10}}>
-                      <Badge bg="secondary" style={{paddingRight: 5}}>
-                        {typeFilter}
-                        <Button style={{ backgroundColor: 'transparent' }} icon="cross" onClick={() => setTypeFilter('')}>X</Button>
-                      </Badge>
-                    </Text>
-                  )}
-                  
-                  <Products selectedFilter={filterObject} products={products} resetFilter={resetFilter}/>
+                  <Products
+                    loader={loader}
+                    selectedFilter={filterObject}
+                    products={products}
+                    resetFilter={resetFilter}
+                    resetTypeFilter={resetTypeFilter}
+                    typeFilter={typeFilter}
+                    emptyCategoryError={emptyCategoryError}
+                  />
                 </>
-                
+
               )}
-              
+
             </div>
           </Col>
-          <Col style={{maxWidth: 10}} className={'backToTop'}>
+          <Col style={{ maxWidth: 10 }} className={'backToTop'}>
             <Button colorScheme='gray' className="bp4-minimal" onClick={() => scrollToTop()}>
               <ChevronUpIcon /> To Top
             </Button>
